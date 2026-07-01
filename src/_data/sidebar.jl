64c088e947cc9a033816ef08c10a09c48dc4fdfe
Quentin G Clark <quentin.gage.clark@gmail.com>
2026-07-01 14:32:16 -0500

    Update sidebar.jl

--- src/_data/sidebar.jl
+++ src/_data/sidebar.jl
@@ -3,6 +3,6 @@
     "module1" => "Module 1: FAQ",
     "module2" => "Module 2: Geological Background",
     "module3" => "Module 3: Updated Course Syllabus", 
-    "module4" => "Data Analysis Notebooks",
+    "module4" => "Seismic Interpretation Notebooks",
     "module5" => "Attribution"
 ]
